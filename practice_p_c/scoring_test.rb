be4310c85610bbf8ce61cd0a17cdabd441a4c9e5
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-08-04 15:30:28 +0900

    refactoring scoring.rb

--- practice_p_c/scoring_test.rb
+++ practice_p_c/scoring_test.rb
@@ -1,17 +1,27 @@
-input_number = gets.chomp.split(" ")
-number_of_students = input_number[0].to_i
-passing_score = input_number[1].to_i
+# frozen_string_literal: true
+
+require 'byebug'
+
+def data_acquisition
+  input_number = gets.chomp.split(' ')
+  @number_a = input_number[0].to_i
+  @number_b = input_number[1].to_i
+end
+
+data_acquisition
+number_of_students = @number_a
+passing_score = @number_b
 
 successful_candidate = []
 rejected_person = []
 
 student_number = 1
 number_of_students.times do
-  info_of_student = gets.chomp.split(" ")
-  score = info_of_student[0].to_i
-  number_of_absences = info_of_student[1].to_i
+  data_acquisition
+  score = @number_a
+  number_of_absences = @number_b
   result = score - (number_of_absences * 5)
-  final_result = result < 0 ? 0 : result
+  final_result = result.negative? ? 0 : result
   if final_result >= passing_score
     successful_candidate << student_number
   else
@@ -20,4 +30,4 @@ number_of_students.times do
   student_number += 1
 end
 
-successful_candidate.each {|stu| puts stu }
+successful_candidate.each { |stu| puts stu }
