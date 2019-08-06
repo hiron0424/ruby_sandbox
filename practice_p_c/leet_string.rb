4941c1adaba4f2b948936d3905fc2dba7a45ccb2
hiroyuki_sakurada <sanno.phill@gmail.com>
2019-08-06 10:42:16 +0900

    create leet_string.rb

---
+++ practice_p_c/leet_string.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+# 016
+
+origin_word = gets.chomp
+
+converted_word = origin_word.gsub(/[AEGIOSZ]/,
+                                  'A' => '4',
+                                  'E' => '3',
+                                  'G' => '6',
+                                  'I' => '1',
+                                  'O' => '0',
+                                  'S' => '5',
+                                  'Z' => '2')
+
+puts converted_word
